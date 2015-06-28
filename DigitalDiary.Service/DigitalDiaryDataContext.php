1fabf3435647080f6effabebc7bebd4989ce68ea
AMuqsit <abdemuqsit@hotmail.com>
2015-06-28 10:32:40 +0500

    Client

--- DigitalDiary.Service/DigitalDiaryDataContext.php
+++ DigitalDiary.Service/DigitalDiaryDataContext.php
@@ -19,6 +19,8 @@ connection = new mysqli(self::servername,self::username,self::password);
             die("Connection failed: " . $this->connection->connect_error);
         }
         $this->createDatabase();
+        $this->selectDatabase();
+        $this->createTables();
     }
 
 
@@ -35,7 +37,36 @@ connection = new mysqli(self::servername,self::username,self::password);
     private function selectDatabase(){
         if ($this->connection->select_db('digitaldiary')!==true)
         {
-        	echo 'Error Selecting Database';
+        	echo 'Error Selecting Database'.$this->connection->error;
+        }
+
+    }
+
+    //To create database tables
+    private function createTables(){
+        $sql='CREATE TABLE IF NOT EXISTS Users(
+            Id INT PRIMARY KEY,
+            UserName  NVARCHAR (20),
+            Password  NVARCHAR (20),
+            FirstName NVARCHAR (30),
+            LastName  NVARCHAR (30),
+            Picture   NVARCHAR (100),
+            PHONE     NVARCHAR (20),
+            ADDRESS   NVARCHAR (255)
+            );
+
+            CREATE TABLE IF NOT EXISTS Entries(
+            Id INT PRIMARY KEY,
+            Title  NVARCHAR (255),
+            Text   TEXT,
+            TIMESTAMP  DATETIME,
+            UserId INT NOT NULL,
+
+            FOREIGN KEY (UserId) REFERENCES Users(Id) on delete cascade on update cascade
+            );';
+
+        if ( $this->connection->multi_query($sql)!==true){
+        	echo 'Error creating tables'.$this->connection->error;
         }
 
     }
