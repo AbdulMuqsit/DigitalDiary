9adeb1c8a43da7f0754b72ab57ab4ffbdf166be9
AMuqsit <abdemuqsit@hotmail.com>
2015-06-29 05:47:31 +0500

    Index view update

--- DigitalDiary.Service/index.php
+++ DigitalDiary.Service/index.php
@@ -1,21 +1,16 @@
-<?php
-require_once('Model/User/User.php');
-require_once('Model/User/UserRepository.php');
-require_once('Model/Entry/Entry.php');
-require_once('Model/Entry/EntryRepository.php');
-require_once('Model/DigitalDiaryDbContext.php');
-require_once('Model/DigitalDiaryDb.php');
+<html>
+    <head>
+        <title>
+            Pend
+        </title>
+    </head>
+    <body>
+        <nav class="nav-bar">
 
-//mysqli_report(MYSQLI_REPORT_ALL);
+        </nav>
+        <?php
 
-$context = new DigitalDiaryDbContext();
+        ?>
 
-$user = new User();
-$user->address="sdafds";
-$user->FirstName="dfg";
-$user->LastName="hdhdfg";
-
-$context->users->add($user);
-$context->users->update(3,$user);
-$context->users->delete(4);
-?>
+    </body>
+</html>
