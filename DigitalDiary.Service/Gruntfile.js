9e2ce01a9cfa4b3b729333df99ceb20d9b98fbf4
AMuqsit <abdemuqsit@hotmail.com>
2015-06-29 13:27:37 +0500

    turnjs update

---
+++ DigitalDiary.Service/Gruntfile.js
@@ -0,0 +1,21 @@
+﻿/*global module */
+module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig({
+        // read in the project settings from the package.json file into the pkg property
+        pkg: grunt.file.readJSON('package.json')
+
+
+    });
+
+    // Add all plugins that your project needs here
+    grunt.loadNpmTasks('');
+
+    // this would be run by typing "grunt test" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('test', []);
+
+    // define the default task that can be run just by typing "grunt" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('default', []);
+};
