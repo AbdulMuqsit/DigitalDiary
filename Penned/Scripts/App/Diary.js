cd630c8038fcaf1acc7ee017660ee9d2ee08ad47
AMuqsit <abdemuqsit@hotmail.com>
2015-06-30 08:28:27 +0500

    changes

---
+++ Penned/Scripts/App/Diary.js
@@ -0,0 +1,20 @@
+﻿
+//After authentication, load user's diary
+(function () {
+    document.getElementById('login').onclick = function () {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("GET", 'Views/Diary.html', true);
+        xhr.onreadystatechange = handle;
+        xhr.send(null);
+
+
+        function handle() {
+            if (xhr.readyState == 4) {
+                document.getElementById('content').innerHTML = xhr.responseText;
+                loadDiary();
+                set();
+            }
+        }
+    };
+})();
